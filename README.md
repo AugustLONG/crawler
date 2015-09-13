826eb43343ffb8ec0fbc356c8af94453786f4c24
taohongxiu <290191473@qq.com>
2015-09-14 02:26:39 +0800

    修改表结构,增加基础表单和专题

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# crawler
-基于scrapy的任务调度系统，采用reids、kafka、celery、rabbitmq、elasticsearch、mysql、django等
+# 大数据搜索
+基于scrapy的采集系统，采用reids、kafka、celery、rabbitmq、elasticsearch、mysql、django等技术
+打造新生代的数据搜索和数据基础平台
--- webapp/alibaba/search/models.py
+++ webapp/alibaba/search/models.py
@@ -1,5 +1,8 @@
 #coding=utf-8
 from django.db import models
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from scraper.models import Scraper, Website
 
 
 class CategoryManager(models.Manager):
@@ -38,10 +41,83 @@ class LinkManager(models.Manager):
         link.save()
         return link
 
+
 class TagManager(models.Manager):
     def hot(self):
         categories = self.model.objects.filter(enabled=True).order_by("-hot")
         return categories
+
+
+class Link(models.Model):
+    name = models.CharField(u"名称", max_length=200)
+    picture = models.ImageField(u"图片", width_field=100, height_field=100, upload_to='photos', blank=True, null=True)
+    url = models.URLField(u"地址")
+    enabled = models.BooleanField(u"是否可用", default=True)
+    order = models.SmallIntegerField(u"序号", default=0)
+    created = models.DateTimeField(u"创建时间", auto_now_add=True, editable=False)
+    updated = models.DateTimeField(u"更新时间", auto_now=True, editable=False)
+    objects = LinkManager()
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = verbose_name = u"友情连接"
+        get_latest_by = "created"
+        ordering = ("order", )
+
+    # @models.permalink
+    def get_absolute_url(self):
+        return self.url
+
+
+class GeneralForms(models.Model):
+    FORM_TYPE = (
+        ('select', 'SELECT'),
+        ('text', 'TEXT'),
+        ('datetime', 'DATETIME'),
+        ('textarea', 'TEXTAREA'),
+        ('date', 'DATE'),
+    )
+    FORM_VALUE_TYPE = (
+        ('char', 'char'),
+        ('email', 'email'),
+        ('datetime', 'DATETIME'),
+        ('ip', 'IP'),
+        ('slug', 'SLUG'),
+        ('int', 'int'),
+        ('date', 'DATE'),
+    )
+    name = models.CharField(u"简单的说明", max_length=200)
+    cname = models.CharField(u"中文", max_length=200, blank=True)
+    slug = models.CharField(u"别名", max_length=200, blank=True)
+    ename = models.CharField(u"英文", max_length=200, blank=True)
+    jname = models.CharField(u"日文", max_length=200, blank=True)
+    kname = models.CharField(u"韩文", max_length=200, blank=True)
+    rname = models.CharField(u"俄语", max_length=200, blank=True)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField(u"对象ID")
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    form_type = models.CharField(u"表单类型", max_length=10, choices=FORM_TYPE)
+    form_value = models.CharField(u"表单值域", max_length=250,  help_text=u"如果是表单类型是select,1:v,2:v2", blank=True)
+    form_attr = models.CharField(u"表单属性", max_length=250, help_text=u"可以是长度限制，样式调整等", blank=True)
+    max_length = models.PositiveIntegerField(u"最大长度", default = 200)
+    min_length = models.PositiveIntegerField(u"最小长度", default = 1)
+    value_type = models.CharField(u"值的类型", default="char", max_length=10, choices=FORM_VALUE_TYPE)
+    help_text = models.CharField(u"帮助", max_length=250)
+    created = models.DateTimeField(u"创建时间", auto_now_add=True, editable=False)
+    updated = models.DateTimeField(u"更新时间", auto_now=True, editable=False)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = verbose_name = u"基础表单"
+
+    # @models.permalink
+    def get_absolute_url(self):
+        return self.pk
+
 
 class Tag(models.Model):
     name = models.CharField(u"名称", max_length=200)
@@ -98,38 +174,42 @@ class Category(models.Model):
         return categories
 
 
-class Link(models.Model):
-    name = models.CharField(u"名称", max_length=200)
-    picture = models.ImageField(u"图片", width_field=100, height_field=100, upload_to='photos', blank=True, null=True)
-    url = models.URLField(u"地址")
-    enabled = models.BooleanField(u"是否可用", default=True)
-    order = models.SmallIntegerField(u"序号", default=0)
+class CategoryForms(models.Model):
+    forms = models.ForeignKey(GeneralForms)
+    category = models.ForeignKey(Category,blank=True, null=True)
+    mustable = models.BooleanField(u"是否必须", default=True)
+    nullable = models.BooleanField(u"是否空", default=False)
+    default_value = models.CharField(u"默认值", max_length=250)
+    htmlable = models.BooleanField(u"允许HTML", default=True)
+    order = models.SmallIntegerField(u"排序", default=0)
+    editable = models.BooleanField(u"是否可以编辑", default=False)
     created = models.DateTimeField(u"创建时间", auto_now_add=True, editable=False)
     updated = models.DateTimeField(u"更新时间", auto_now=True, editable=False)
-    objects = LinkManager()
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = verbose_name = u"友情连接"
-        get_latest_by = "created"
-        ordering = ("order", )
+        verbose_name_plural = verbose_name = u"分类表单"
 
     # @models.permalink
     def get_absolute_url(self):
-        return self.url
+        return self.slug
 
-from scraper.models import Scraper, Website
+
 class PageModule(models.Model):
     CHECKER_TYPE = (
         ('L', 'LIST'),
         ('D', 'DETAIL'),
         ('I', 'INDEX'),
     )
-    name = models.CharField(u"页面名称", max_length=200)
+    name = models.CharField(u"名称", max_length=200,blank=True)
+    title = models.CharField(u"页面标题", max_length=200,blank=True)
+    keywords = models.CharField(u"关键词", max_length=200,blank=True)
+    description = models.CharField(u"页面描述", max_length=200,blank=True)
     slug = models.SlugField(u"别名")
     scraper = models.ForeignKey(Scraper)
+    category = models.ForeignKey(Category,blank=True, null=True)
     style = models.CharField(u"CSS样式地址", max_length=200)
     js = models.CharField(u"js地址", max_length=200)
     page_type = models.CharField(max_length=1, choices=CHECKER_TYPE, default='L')
@@ -143,7 +223,7 @@ class PageModule(models.Model):
         return self.name
 
     class Meta:
-        verbose_name_plural = verbose_name = u"友情连接"
+        verbose_name_plural = verbose_name = u"页面模块"
         get_latest_by = "created"
 
     # @models.permalink
@@ -156,3 +236,24 @@ class PageModule(models.Model):
     def style_path(self):
         return self.style.split(",")
 
+
+class Topic(models.Model):
+    name = models.CharField(u"名称", max_length=200)
+    title = models.CharField(u"页面标题", max_length=200)
+    keywords = models.CharField(u"关键词", max_length=200)
+    description = models.CharField(u"页面描述", max_length=200, blank=True, null=True)
+    slug = models.SlugField(u"别名")
+    category = models.ForeignKey(Category,blank=True, null=True)
+    style = models.CharField(u"CSS样式地址", max_length=200)
+    js = models.CharField(u"js地址", max_length=200)
+    template = models.TextField(u"模板", blank=True, help_text=u"可以是渲染动态或者静态模板")
+    url = models.CharField(u"模板地址", max_length=200, blank=True, help_text=u"如果模板和地址都存在，则地址是独立的，可以直接访问")
+    enabled = models.BooleanField(u"是否可用", default=True)
+    created = models.DateTimeField(u"创建时间", auto_now_add=True, editable=False)
+    updated = models.DateTimeField(u"更新时间", auto_now=True, editable=False)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = verbose_name = u"专题"
