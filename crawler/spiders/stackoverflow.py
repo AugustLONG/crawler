32d42a505f2107046afe89ba79d2a7e617952c31
taohongxiu <290191473@qq.com>
2015-11-21 01:16:23 +0800

    增加测试stackflow采集

--- crawler/spiders/stackoverflow.py
+++ crawler/spiders/stackoverflow.py
@@ -2,116 +2,133 @@
 from scrapy.contrib.linkextractors import LinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy import Request
-import re, json
+import re, json, time
 from pymongo import MongoClient
 
 mongodb = MongoClient()
 stackoverflowdb = mongodb.stackoverflow
+key = "&key=U4DMV*8nvpm3EOpvf69Rxw(("
 
 
 class StackoverflowSpider(CrawlSpider):
     name = "stackoverflow"
     allowed_domains = ["stackoverflow.com", "stackexchange.com"]
-    start_urls = [
-        "http://api.stackexchange.com/2.2/questions?page=1&pagesize=50&order=desc&sort=activity&site=stackoverflow&filter=!9YdnSQHvY",
-        "http://api.stackexchange.com/2.2/answers?order=desc&sort=reputation&site=stackoverflow&filter=!9YdnSQHvY",
-        "http://api.stackexchange.com/2.2/users?order=desc&sort=reputation&site=stackoverflow&filter=!9YdnSQHvY",
-        "http://api.stackexchange.com/2.2/comments?order=desc&sort=reputation&site=stackoverflow&filter=!9YdnSQHvY"
-    ]
+    question_url = "http://api.stackexchange.com/2.2/questions/%s?order=desc&sort=activity&site=stackoverflow&filter=!0Uv6ZT)teRUpIDUXg)eKBUB)K" + key
+    comment_url = "http://api.stackexchange.com/2.2/comments/%s?order=desc&sort=creation&site=stackoverflow&filter=!-*f(6sqoFKmM" + key
+    answer_url = "http://api.stackexchange.com/2.2/answers/%s?order=desc&sort=activity&site=stackoverflow&filter=!LUcFBdXEo-6Qzavr4yj3rc" + key
+    user_url = "http://api.stackexchange.com/2.2/users/%s?order=desc&sort=reputation&site=stackoverflow&filter=!-*f(6q9aL0dv" + key
 
-    def parse(self, response):
-        items = json.loads(response.content)
-        if "items" in items:
-            for item in items:
-                owner_id = item["owner"]["user_id"]
-                item["owner_id="] = owner_id
-                question_id = item["question_id"]
-                del item["owner"]
-                stackoverflowdb.question.update({"question_id": question_id}, {"$set": item}, upsert=True)
-                yield Request(
-                    "http://api.stackexchange.com/2.2/users/%s?order=desc&sort=reputation&site=stackoverflow&filter=!-*f(6q9aL0dv" % owner_id,
-                    callback=self.parse_users)
-                yield Request(
-                    "http://api.stackexchange.com/2.2/questions/%s?order=desc&sort=activity&site=stackoverflow&filter=!0Uv6ZT)teRUpIDUXg)eKBUB)K" % question_id,
-                    callback=self.parse_questions)
+    def start_requests(self):
+        for i in range(1, 500, 1):
+            i = str(i)
+            yield Request(
+                'http://api.stackexchange.com/2.2/questions?page=' + i + '&pagesize=50&order=desc&sort=activity&site=stackoverflow&filter=!LUcFBE)pRrTKav*c.B*_WQ' + key,
+                self.parse_questions)
+            yield Request(
+                'http://api.stackexchange.com/2.2/answers?page=' + i + '&pagesize=50&order=desc&sort=activity&site=stackoverflow&filter=!)rCcH8tl1NCISRRturSV' + key,
+                self.parse_answers)
+            yield Request(
+                'http://api.stackexchange.com/2.2/users?page=' + i + '&pagesize=50&order=desc&sort=reputation&site=stackoverflow&filter=!-*f(6q9aL0dv' + key,
+                self.parse_users)
+            yield Request(
+                'http://api.stackexchange.com/2.2/comments?page=' + i + '&pagesize=50&order=desc&sort=creation&site=stackoverflow&filter=!b0OfN.wWgRBMab' + key,
+                self.parse_comments)
+            yield Request(
+                'http://api.stackexchange.com/2.2/tags?page=' + i + '&order=desc&sort=popular&site=stackoverflow&filter=!-*f(6qOIRTkE' + key,
+                self.parse_tags)
+
+
+    def delay(self, seconds=10):
+        time.sleep(seconds)
+
+    def _parse_all(self, comments=[], questions=[], users=[], answers=[]):
+        for comment in comments:
+            stackoverflowdb.comment.update({"comment_id": comment["comment_id"]}, {"$set": comment}, upsert=True)
+            yield Request(self.comment_url % comment["comment_id"], callback=self.parse_comments)
+        for question in questions:
+            stackoverflowdb.question.update({"question_id": question["question_id"]}, {"$set": question}, upsert=True)
+            yield Request(self.question_url % question["question_id"], callback=self.parse_questions)
+        for answer in answers:
+            stackoverflowdb.answer.update({"answer_id": answer["answer_id"]}, {"$set": answer}, upsert=True)
+            yield Request(self.answer_url % answer["answer_id"], callback=self.parse_answers)
+        for user in users:
+            stackoverflowdb.user.update({"user_id": user["user_id"]}, {"$set": user}, upsert=True)
+            yield Request(self.user_url % user["user_id"], callback=self.parse_users)
+
+    def _parse_item(self, item, comments=[], questions=[], users=[], answers=[]):
+        if "answers" in item:
+            answers.extend(item["answers"])
+            del item["answers"]
+        if "comments" in item:
+            comments.extend(item["comments"])
+            del item["comments"]
+        if "questions" in item:
+            questions.extend(item["questions"])
+            del item["questions"]
+        if "owner" in item:
+            users.append(item["owner"])
+            del item["owner"]
+        if "reply_to_user" in item:
+            users.append(item["reply_to_user"])
+            del item["reply_to_user"]
+        if "last_editor" in item:
+            users.append(item["last_editor"])
+            item["last_editor_id"]=item["last_editor"]["user_id"]
+            del item["last_editor"]
+        if "bounty_user" in item:
+            users.append(item["bounty_user"])
+            item["bounty_user_id"]=item["bounty_user"]["user_id"]
+            del item["bounty_user"]
+        if "awarded_bounty_users" in item:
+            users.extend(item["awarded_bounty_users"])
+            del item["awarded_bounty_users"]
+        return item, comments, questions, users, answers
 
     def parse_users(self, response):
-        items = json.loads(response.content)
+        self.delay()
+        items = json.loads(response.body)
         if "items" in items:
-            for item in items:
-                user_id = item["user_id"]
-                stackoverflowdb.user.update({"user_id": user_id}, {"$set": item}, upsert=True)
-                if "answers" in item:
-                    for answer in item["answers"]:
-                        yield Request(
-                            "http://api.stackexchange.com/2.2/answers/%s?order=desc&sort=activity&site=stackoverflow&filter=!LUcFBIwXAv-E7rc11-bLO." %
-                            answer["answer_id"], callback=self.parse_answers)
-                    del item["answers"]
-                if "comments" in item:
-                    for comment in item["comments"]:
-                        yield Request(
-                            "http://api.stackexchange.com/2.2/comments/%s?order=desc&sort=creation&site=stackoverflow&filter=!b0OfN.wWgRBMab" %
-                            comment["comment_id"], callback=self.parse_comments)
-                    del item["comments"]
-                if "questions" in item:
-                    for question in item["questions"]:
-                        yield Request(
-                            "http://api.stackexchange.com/2.2/questions/%s?order=desc&sort=activity&site=stackoverflow&filter=!0Uv6ZT)teRUpIDUXg)eKBUB)K" %
-                            question["question_id"], callback=self.parse_questions)
-                    del item["questions"]
+            answers, comments, questions, users = [], [], [], []
+            for item in items["items"]:
+                item, comments, questions, users, answers = self._parse_item(item, comments, questions, users, answers)
+                stackoverflowdb.user.update({"user_id": item["user_id"]}, {"$set": item}, upsert=True)
+            self._parse_all(comments=comments, questions=questions, answers=answers, users=users)
 
     def parse_questions(self, response):
-        items = json.loads(response.content)
+        self.delay()
+        items = json.loads(response.body)
         if "items" in items:
-            for item in items:
-                owner_id = item["owner"]["user_id"]
-                item["owner_id="] = owner_id
-                question_id = item["question_id"]
-                del item["owner"]
-                stackoverflowdb.question.update({"question_id": question_id}, {"$set": item}, upsert=True)
-                yield Request(
-                    "http://api.stackexchange.com/2.2/users/%s?order=desc&sort=reputation&site=stackoverflow&filter=!-*f(6q9aL0dv" % owner_id,
-                    callback=self.parse_users)
-                if "answers" in item:
-                    for answer in item["answers"]:
-                        yield Request(
-                            "http://api.stackexchange.com/2.2/answers/%s?order=desc&sort=activity&site=stackoverflow&filter=!LUcFBIwXAv-E7rc11-bLO." %
-                            answer["answer_id"], callback=self.parse_answers)
-                    del item["answers"]
-                if "comments" in item:
-                    for comment in item["comments"]:
-                        yield Request(
-                            "http://api.stackexchange.com/2.2/comments/%s?order=desc&sort=creation&site=stackoverflow&filter=!b0OfN.wWgRBMab" %
-                            comment["comment_id"], callback=self.parse_comments)
-                    del item["comments"]
+            answers, comments, questions, users = [], [], [], []
+            for item in items["items"]:
+                item, comments, questions, users, answers = self._parse_item(item, comments, questions, users, answers)
+                stackoverflowdb.question.update({"question_id": item["question_id"]}, {"$set": item}, upsert=True)
+            self._parse_all(comments=comments, questions=questions, answers=answers, users=users)
 
     def parse_answers(self, response):
-        items = json.loads(response.content)
+        self.delay()
+        items = json.loads(response.body)
         if "items" in items:
-            for item in items:
-                owner_id = item["owner"]["user_id"]
-                item["owner_id="] = owner_id
-                answer_id = item["answer_id"]
-                stackoverflowdb.answer.update({"answer_id": answer_id}, {"$set": item}, upsert=True)
-                yield Request(
-                    "http://api.stackexchange.com/2.2/users/%s?order=desc&sort=reputation&site=stackoverflow&filter=!-*f(6q9aL0dv" % owner_id,
-                    callback=self.parse_users)
+            answers, comments, questions, users = [], [], [], []
+            for item in items["items"]:
+                item, comments, questions, users, answers = self._parse_item(item, comments, questions, users, answers)
+                stackoverflowdb.answer.update({"answer_id": item["answer_id"]}, {"$set": item}, upsert=True)
+            self._parse_all(comments=comments, questions=questions, answers=answers, users=users)
 
     def parse_comments(self, response):
-        items = json.loads(response.content)
+        self.delay()
+        items = json.loads(response.body)
         if "items" in items:
-            for item in items:
-                owner_id = item["owner"]["user_id"]
-                item["owner_id="] = owner_id
-                item["reply_to_user_id"] = reply_to_user_id
-                reply_to_user_id = item["reply_to_user"]["user_id"]
-                comment_id = item["comment_id"]
-                del item["owner"]
-                del item["reply_to_user"]
-                stackoverflowdb.comment.update({"comment_id": comment_id}, {"$set": item}, upsert=True)
-                yield Request(
-                    "http://api.stackexchange.com/2.2/users/%s?order=desc&sort=reputation&site=stackoverflow&filter=!-*f(6q9aL0dv" % owner_id,
-                    callback=self.parse_users)
-                yield Request(
-                    "http://api.stackexchange.com/2.2/users/%s?order=desc&sort=reputation&site=stackoverflow&filter=!-*f(6q9aL0dv" % reply_to_user_id,
-                    callback=self.parse_users)
+            answers, comments, questions, users = [], [], [], []
+            for item in items["items"]:
+                item, comments, questions, users, answers = self._parse_item(item, comments, questions, users, answers)
+                stackoverflowdb.comment.update({"comment_id": item["comment_id"]}, {"$set": item}, upsert=True)
+            self._parse_all(comments=comments, questions=questions, answers=answers, users=users)
+
+    def parse_tags(self, response):
+        self.delay(3)
+        items = json.loads(response.body)
+        if "items" in items:
+            for item in items["items"]:
+                stackoverflowdb.tags.update({"name": item["name"]}, {"$set": item}, upsert=True)
+
+
