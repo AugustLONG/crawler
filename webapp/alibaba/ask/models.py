2a6e9f66c543d2256e4cd3dee7ed80dd3ea780d2
ff_li <ff_li@ctrip.com>
2015-11-27 19:33:58 +0800

    add other modules

---
+++ webapp/alibaba/ask/models.py
@@ -0,0 +1,5 @@
+# encoding:utf-8
+import datetime
+
+from django.core.urlresolvers import reverse
+from django.db import models
---
+++ webapp/alibaba/soft/views.py
@@ -0,0 +1,2 @@
+from django.conf import settings
+from django.shortcuts import render_to_response,RequestContext
